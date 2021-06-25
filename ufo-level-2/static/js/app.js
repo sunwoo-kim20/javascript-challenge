7d613eb385f8742cd1a666ad002ea29a0af7e569
Sunwoo Kim <s.kim32415@gmail.com>
2021-06-25 16:25:48 -0700

    created function to create table

--- ufo-level-2/static/js/app.js
+++ ufo-level-2/static/js/app.js
@@ -14,18 +14,22 @@ var form = d3.select("#form");
 // Getting a reference to the button
 var button = d3.select("#filter-btn");
 
+// Create function for appending table rows
+function createTable(inputData) {
+  inputData.forEach(sighting =>
+    {var row = tbody.append("tr");
+    row.append("td").text(sighting.datetime);
+    row.append("td").text(titleCase(sighting.city));
+    row.append("td").text(sighting.state.toUpperCase());
+    row.append("td").text(sighting.country.toUpperCase());
+    row.append("td").text(sighting.shape);
+    row.append("td").text(sighting.durationMinutes);
+    row.append("td").text(sighting.comments);
+    }
+  );
+}
 // Adding rows of data to table
-data.forEach(sighting =>
-  {var row = tbody.append("tr");
-  row.append("td").text(sighting.datetime);
-  row.append("td").text(titleCase(sighting.city));
-  row.append("td").text(sighting.state.toUpperCase());
-  row.append("td").text(sighting.country.toUpperCase());
-  row.append("td").text(sighting.shape);
-  row.append("td").text(sighting.durationMinutes);
-  row.append("td").text(sighting.comments);
-  }
-);
+createTable(data);
 
 
 // Create event handlers
@@ -39,17 +43,7 @@ function searchDates() {
 
   // Start with a full table
   tbody.html("");
-  data.forEach(sighting =>
-    {var row = tbody.append("tr");
-    row.append("td").text(sighting.datetime);
-    row.append("td").text(titleCase(sighting.city));
-    row.append("td").text(sighting.state.toUpperCase());
-    row.append("td").text(sighting.country.toUpperCase());
-    row.append("td").text(sighting.shape);
-    row.append("td").text(sighting.durationMinutes);
-    row.append("td").text(sighting.comments);
-    }
-  );
+  createTable(data);
 
   // Getting a reference to the input
   var dateInput = d3.select("#datetime").node().value;
@@ -81,15 +75,5 @@ function searchDates() {
 
   // Create new table
   tbody.html("");
-  newData.forEach(sighting =>
-    {var row = tbody.append("tr");
-    row.append("td").text(sighting.datetime);
-    row.append("td").text(titleCase(sighting.city));
-    row.append("td").text(sighting.state.toUpperCase());
-    row.append("td").text(sighting.country.toUpperCase());
-    row.append("td").text(sighting.shape);
-    row.append("td").text(sighting.durationMinutes);
-    row.append("td").text(sighting.comments);
-    }
-  );
+  createTable(newData);
 }
